5a0bc11d0fcb81e4d6d6a03048a4dbc4561c3d5f
elukyanchikova <lukyanchikowae@gmail.com>
2020-04-06 16:32:42 +0300

    add TP3

---
+++ assignments/e.lukyanchikova/elukyanchikovaTP3/StrasbourgPepperroniPizza.java
@@ -0,0 +1,7 @@
+package elukyanchikovaTP3;
+
+public class StrasbourgPepperroniPizza extends Pizza {
+    public StrasbourgPepperroniPizza() {
+        this.pizzaName = "StrasbourgPepperoni";
+    }
+}
