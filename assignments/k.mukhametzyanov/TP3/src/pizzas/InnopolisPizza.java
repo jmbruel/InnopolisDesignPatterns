a2e57689c222314cc4ece837e5b1767845834bf8
mukhametzyanovk <47362134+mukhametzyanovk@users.noreply.github.com>
2020-04-06 22:55:20 +0300

    pizzas

---
+++ assignments/k.mukhametzyanov/TP3/src/pizzas/InnopolisPizza.java
@@ -0,0 +1,10 @@
+package pizzas;
+
+public class InnopolisPizza extends Pizza {
+    public InnopolisPizza() {
+        this.name = "Pizza from Innopolis with love!";
+        this.toppings.add("Red pepper");
+        this.toppings.add("Green pepper");
+        this.toppings.add("Bolognese");
+    }
+}
