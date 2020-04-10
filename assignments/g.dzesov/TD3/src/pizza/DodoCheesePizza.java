5828a9ccee661fc756f01c899b6ac7b72254fb38
dzesov <roT}gGd#9DFzfIf314!mK{>
2020-04-10 09:00:28 +0300

    closes #32

---
+++ assignments/g.dzesov/TD3/src/pizza/DodoCheesePizza.java
@@ -0,0 +1,7 @@
+package main.pizza;
+
+public class DodoCheesePizza extends Pizza {
+    public DodoCheesePizza(){
+        this.name = "Dodo Cheeze pizza";
+    }
+}
---
+++ assignments/g.dzesov/TD3/src/pizza/DodoHawaiianPizza.java
@@ -0,0 +1,7 @@
+package main.pizza;
+
+public class DodoHawaiianPizza extends Pizza {
+    public DodoHawaiianPizza(){
+        this.name = "Dodo HawaiianPizza pizza";
+    }
+}
