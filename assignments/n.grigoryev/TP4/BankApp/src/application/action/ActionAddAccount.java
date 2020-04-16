8d2eae54f5a6d3b3cecdd77b1076bed0cbb7a103
grinikita <Grigoriev98>
2020-04-17 02:20:31 +0300

    [TP4] n.grigoryev Improve architecture

---
+++ assignments/n.grigoryev/TP4/BankApp/src/application/action/ActionAddAccount.java
@@ -0,0 +1,45 @@
+package application.action;
+
+import application.actioncontext.BankActionContext;
+import bank.Account;
+import bank.BankAgency;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class ActionAddAccount implements Action<BankActionContext> {
+    private String message;
+    private String code;
+
+    public ActionAddAccount(String code, String message) {
+        this.message = message;
+        this.code = code;
+    }
+
+    @Override
+    public String actionMessage() {
+        return this.message;
+    }
+
+    @Override
+    public String actionCode() {
+        return this.code;
+    }
+
+    @Override
+    public void execute(BankActionContext context) throws Exception {
+        Scanner scanner = context.getScanner();
+        PrintStream printStream = context.getPrintStream();
+        BankAgency ag = context.getBankAgency();
+        printStream.print("Account Number -> ");
+        String number = scanner.next();
+        printStream.print("Account owner name -> ");
+        String name = scanner.next();
+        Account c = new Account(number, name);
+        try {
+            ag.addAccount(c);
+        } catch (Exception e) {
+            printStream.println(e.toString());
+        }
+    }
+}
