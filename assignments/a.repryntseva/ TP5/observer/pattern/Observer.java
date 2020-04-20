8165c19bd379d992508fb94cfa0e79a01b1eb4d8
Anastasia Reprintseva <a.reprintseva@tinkoff.ru>
2020-04-19 20:38:53 -0800

    TP5

---
+++ assignments/a.repryntseva/ TP5/observer/pattern/Observer.java
@@ -0,0 +1,22 @@
+package pattern;
+
+/**
+ * An interface for all Observers
+ */
+public interface Observer {
+	/**
+	 * Informs this observer that an observed subject has changed
+	 * 
+	 * @param o
+	 *            the observed subject that has changed
+	 */
+	public void update(Observable o);
+
+	/**
+	 * Informs this observer that an observed subject has changed
+	 *
+	 * @param object - the change
+	 *            the observed subject that has changed
+	 */
+	public void update(Object object);
+}
